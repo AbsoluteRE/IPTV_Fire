119270258c8ea7c4343ef8ce84217cfd928c8cce
Lunxry Officiel <Digytal.Ravazer974@gmail.com>
2025-05-10 19:42:50 +0000

    il faudrait qu'on puisse ajouter des liens comme ceci également : http://vip.sipderott4k.info:14820/get.php?username=656766&password=56568887&type=m3u_plus&output=ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
-// Regex for validating Xtream Codes API URL (http://host:port)
-export const XTREAM_CODES_URL_REGEX = /^http:\/\/[a-zA-Z0-9.-]+:\d+$/;
+// Regex for validating Xtream Codes API URL (http://host:port/path?query)
+export const XTREAM_CODES_URL_REGEX = /^http:\/\/[a-zA-Z0-9.-]+:\d+(\/[\w.-]*)*(\?.*)?$/;
 
 // Regex for validating M3U/M3U8 URL
 export const M3U_URL_REGEX = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?(\?.*)?$/i;
+
